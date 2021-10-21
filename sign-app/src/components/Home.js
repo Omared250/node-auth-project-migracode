ab00eb18d5c8d889f28fc61f0a5207862533452b
Omar <omared250@gmail.com>
2021-10-22 00:08:44 +0200

    working new pages

--- sign-app/src/components/Home.js
+++ sign-app/src/components/Home.js
@@ -1,12 +1,15 @@
 import React from "react";
-import SingUp from "./SingUp";
+import SingIn from "./SingIn";
 
 const Home = () => {
     return (
         <div className='home-page'>
             <h1>A space for everyone!</h1>
             <p>Millions of help for your web development. You don't need to have experience!</p>
-            <SingUp/>
+            <div className='buttons'>
+                <button>Sing In!</button>
+                <button>Sing Up!</button>
+            </div>
         </div>
     );
 }
